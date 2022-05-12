7d8a0dcd89b08fc2f40dc72730d605eff6579c53
Samuel Adeyemi <samueladeyemi2006@gmail.com>
2022-05-12 09:32:05 +0100

    initial commit

---
+++ admin-dashboard/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="text-pink-500 text-center">Admin dashboard-capstone gp29</div>;
+}
+
+export default App;
---
+++ admin-dashboard/src/components/Auth/signin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function signin() {
+  return (
+    <div>signin</div>
+  )
+}
+
+export default signin
