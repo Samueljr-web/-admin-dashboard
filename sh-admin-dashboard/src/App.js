e995f2f2bc919d2f059db12822cbb687cd15b2f8
Samuel Adeyemi <samueladeyemi2006@gmail.com>
2022-05-12 21:14:10 +0100

    added all files

---
+++ sh-admin-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+     <div className="text-pink-500 text-center">
+       <h2>
+        Admin dashboard-capstone gp29
+      </h2>
+    </div>
+  )
+}
+
+export default App;
